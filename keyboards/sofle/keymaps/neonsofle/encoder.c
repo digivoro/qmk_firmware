ed7417df316d88518617e8ec25a63ff2f32d2a5d
Felipe Castro Richter <felipe@pegasconsentido.cl>
2021-11-11 02:34:06 -0300

    commit inicial

---
+++ keyboards/sofle/keymaps/neonsofle/encoder.c
@@ -0,0 +1,19 @@
+// kb: sofle
+// km: neonsofle
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_PGUP);
+        } else {
+            tap_code(KC_PGDN);
+        }
+    }
+    return true;
+}
