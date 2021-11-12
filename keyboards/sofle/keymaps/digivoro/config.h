dae3174b64802b20e0ab51d5a92421844217e4ad
Felipe Castro Richter <felipe@pegasconsentido.cl>
2021-11-12 12:15:43 -0300

    Agrego config personal!

---
+++ keyboards/sofle/keymaps/digivoro/config.h
@@ -0,0 +1,64 @@
+// kb: sofle
+// km: neonsofle
+
+#pragma once
+
+// #define OLED_TIMEOUT 60000
+
+#define OLED_DISABLE_TIMEOUT
+
+#define SPLIT_OLED_ENABLE
+
+#define SPLIT_WPM_ENABLE
+
+#define EE_HANDS
+
+#define RGB_DI_PIN D3
+
+#undef TAPPING_TERM
+
+#define TAPPING_TERM 300 // el doble pulso del Tap Dance debe hacerse en menos de 400ms
+
+#define OLED_BRIGHTNESS 120
+
+#ifdef RGBLIGHT_ENABLE // si la linea RGBLIGHT_ENABLE = yes esta en rules.mk
+
+    #undef RGBLED_NUM
+       
+    #define RGBLED_NUM 74 // numero de LEDs en el teclado
+    
+    #undef RGBLED_SPLIT
+    
+    #define RGBLED_SPLIT \
+        {37,37}
+       
+    #define RGBLIGHT_EFFECT_BREATHING
+    // incluyendo el efecto RGB breathing
+    
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    // incluyendo el efecto RGB rainbow mood
+    
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+               
+    #define RGBLIGHT_LIMIT_VAL 110
+    // maximo valor para Value en HSV
+    
+    #define RGBLIGHT_HUE_STEP 5
+    // valor del step al modificar Hue
+    
+    #define RGBLIGHT_SAT_STEP 17
+    // valor del step al modificar Saturation
+    
+    #define RGBLIGHT_VAL_STEP 17
+    // valor del step al modificar Value
+    
+#endif
+
+#define NO_ACTION_MACRO 
+// ya que tengo EXTRAFLAGS += -flto en rules.mk
+
+#define NO_ACTION_FUNCTION 
+// ya que tengo EXTRAFLAGS += -flto en rules.mk
+
+#define NO_ACTION_ONESHOT
+// reduce size al firmware
---
+++ keyboards/sofle/keymaps/digivoro/rules.mk
@@ -0,0 +1,34 @@
+# rules.mk no esta en lenguaje C
+
+# kb: sofle
+# km: neonsofle
+
+OLED_ENABLE = yes
+
+OLED_DRIVER = SSD1306
+
+RGBLIGHT_ENABLE = yes
+
+TAP_DANCE_ENABLE = yes
+# necesario para ocupar el doble pulso (tap dance) en el keymap
+
+EXTRAKEY_ENABLE = yes
+# necesario para ocupar los keycodes KC_MUTE, KC_VOLD y KC_VOLU
+
+MOUSEKEY_ENABLE = no
+
+WPM_ENABLE = yes
+# necesario para ocupar get_current_wpm()
+# obtener el valor de words per minute
+
+LTO_ENABLE = yes
+# Link Time Optimization reduce el size del archivo hex
+
+EXTRAFLAGS += -flto 
+# esta linea reduce considerable size al hex
+
+CONSOLE_ENABLE = no
+
+COMMAND_ENABLE = no
+
+ENCODER_ENABLE = yes
