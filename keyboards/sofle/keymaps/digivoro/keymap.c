e4be8fe5d4709d41d4338738e0bc8d770e8bea5b
Felipe Castro Richter <felipe@pegasconsentido.cl>
2021-11-19 14:35:15 -0300

    Agrego layer numerico

--- keyboards/sofle/keymaps/digivoro/keymap.c
+++ keyboards/sofle/keymaps/digivoro/keymap.c
@@ -4,15 +4,16 @@ enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
     _DVORAK,
+    _NUMERIC,
     _LOWER,
     _RAISE,
-    _ADJUST,
-    _NUMERIC
+    _ADJUST
 };
 
 enum custom_keycodes {
     KC_QWERTY = SAFE_RANGE,
     KC_DVORAK,
+    KC_NUMER,
     KC_LOWER,
     KC_RAISE,
     KC_ADJUST,
@@ -73,15 +74,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_LGUI,  KC_LALT, KC_LCTRL, KC_LOWER,   KC_SPC,     KC_ENT, KC_RAISE, KC_RCTRL,  KC_RALT,  KC_RGUI
 ),
 
+/* NUMERIC
+ * ,----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      | CALC |                    |   /  |   7  |   8  |   9  |  -   |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   ,  |   .  |   $  |   %  |   =  |-------.    ,-------|   *  |   4  |   5  |   6  |  +   |      |   
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |   (  |   )  |-------|    |-------|   0  |   1  |   2  |   3  |  .   |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Space  /       \Enter \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+[_NUMERIC] = LAYOUT(
+  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  KC_CALC,                        KC_PSLS,    KC_P7,    KC_P8,    KC_P9,  KC_PMNS,  _______,
+  _______,  KC_COMM,   KC_DOT,   KC_DLR,  KC_PERC,  KC_PEQL,                        KC_PAST,    KC_P4,    KC_P5,    KC_P6,  KC_PPLS,  _______,
+  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,    _______,    KC_P0,    KC_P1,    KC_P2,    KC_P3,  XXXXXXX,  _______,
+                      KC_LGUI,  KC_LALT, KC_LCTRL, KC_LOWER,   KC_SPC,     KC_ENT, KC_RAISE, KC_RCTRL,  KC_RALT,  KC_RGUI
+),
+
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | F12  |
+ * |      |      |   %  |   ~  |   `  |   $  |                    |   |  |   :  |   =  |      |      | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   /  |   ?  |   \  |   |  |
+ * |  Tab |   !  |   _  |   +  |   {  |   }  |-------|    |-------|   [  |   ]  |   /  |   &  |   ?  |   "  |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * | Shift|   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   (  |   )  |   *  |   ^  |   &  | Shift|
+ * | Shift|   @  |   #  |   -  |   (  |   )  |-------.    ,-------|   <  |   >  |   *  |   ^  |   \  | Shift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR |LOWER | /       /       \      \  |RAISE | RCTR | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -89,10 +112,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                          KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,
-   KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                           KC_6,     KC_7,     KC_8,     KC_9,     KC_0,   KC_F12,
-  _______,   KC_EQL,  KC_MINS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  _______,    _______,  KC_LBRC,  KC_RBRC,  KC_SLSH,  KC_QUES,  KC_BSLS,  KC_PIPE,
-  _______,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,                        KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  _______,
-                      _______,  _______,  _______,  _______,  _______,    _______,  _______, _______, _______, _______
+  _______,  XXXXXXX,  KC_PERC,  KC_TILD,   KC_GRV,   KC_DLR,                        KC_PIPE,  KC_COLN,   KC_EQL,  XXXXXXX,  XXXXXXX,   KC_F12,
+  _______,  KC_EXLM,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,                        KC_LBRC,  KC_RBRC,  KC_SLSH,  KC_AMPR,  KC_QUES,   KC_DQT,
+  _______,    KC_AT,  KC_HASH,  KC_MINS,  KC_LPRN,  KC_RPRN,  _______,    _______,  KC_LABK,  KC_RABK,  KC_ASTR,  KC_CIRC,  KC_BSLS,  _______,
+                      _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______
 ),
 
 /* RAISE
@@ -117,35 +140,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______
 ),
 
-/* NUMERIC
- * ,----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |   7  |   8  |   9  |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |   4  |   5  |   6  |      |      |   
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   0  |   1  |   2  |   3  |      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |      | /       /       \      \  |      |      |      |      |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            `----------------------------------'           '------''---------------------------'
- */
-[_NUMERIC] = LAYOUT(
-  _______,  _______,  _______,  _______,  _______,  _______,                        _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,                        _______,     KC_7,     KC_8,     KC_9,  _______,  _______,
-  _______,  _______,  _______,  _______,  XXXXXXX,  _______,                        _______,     KC_4,     KC_5,     KC_6,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  _______,    _______,     KC_0,     KC_1,     KC_2,     KC_3,  _______,  _______,
-                      _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______
-),
-
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | RESET|      |QWERTY|DVORAK|      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |MACWIN|      |      |      |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
+ * |      |      |MACWIN| NUMER|      |      |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      | PREV | PLAY | NEXT |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -156,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT(
   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   RESET  ,  XXXXXXX,KC_QWERTY,KC_DVORAK,  CG_TOGG,  XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX,  XXXXXXX,  CG_TOGG,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
+  XXXXXXX,  XXXXXXX,  CG_TOGG, KC_NUMER,  XXXXXXX,  XXXXXXX,                       XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
                       _______,  _______,  _______,  _______,  _______,    _______, _______, _______, _______, _______
 )
@@ -350,6 +351,9 @@ static void print_status_narrow(void) {
         case _DVORAK:
             oled_write("DVRAK", false);
             break;
+        case _NUMERIC:
+            oled_write("NUMER", false);
+            break;
         default:
             oled_write("UNDEF", false);
     }
@@ -362,6 +366,7 @@ static void print_status_narrow(void) {
     switch (get_highest_layer(layer_state)) {
         case _DVORAK:
         case _QWERTY:
+        case _NUMERIC:
             oled_write("BASE ", false);
             break;
         case _RAISE:
@@ -412,6 +417,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_DVORAK:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_DVORAK);
+            }
+            return false;
+        case KC_NUMER:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_NUMERIC);
             }
             return false;
         case KC_LOWER:
